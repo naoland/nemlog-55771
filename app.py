711128016b142be9097de2d8689fa7b405855a4c
naoland <73999877+naoland@users.noreply.github.com>
2021-02-11 17:49:51 +0800

    add sample code

---
+++ app.py
@@ -0,0 +1,31 @@
+from bottle import route, run, template
+from zaifapi import ZaifPublicApi, ZaifFuturesPublicApi, ZaifLeverageTradeApi
+
+
+@route("/")
+def index():
+    return "<h1>Hello World!</h1>"
+
+
+@route("/hello/<name>")
+def index2(name):
+    return template("<h1>Hello {{name}}!</h1>", name=name)
+
+
+@route("/last_price/<pair>")
+def last_price(pair):
+    """指定した通貨ペア（xem_jpyなど）の最終価格を表示します。"""
+    zaif = ZaifPublicApi()
+    result = zaif.last_price(pair)
+    print(result)
+    return template(
+        "<h1>{{pair}} 最終価格: {{result}}</h1>", pair=pair, result=result["last_price"]
+    )
+
+
+# host_address = "localhost"
+host_address = "0.0.0.0"
+
+# アプリケーションサーバーを起動します。
+# ホストのアドレスが 0.0.0.0 の場合は、ローカルネットワークのどのPCからでもアクセスできます
+run(host=host_address, port=8080, debug=True, reloader=True)
